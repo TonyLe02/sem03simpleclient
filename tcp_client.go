c4343fe2101ca4c7a82c7fafd432e52c9931af2c
TonyLe02 <tonynl@student.uia.no>
2023-04-23 19:51:42 +0200

    Update tcp_client.go

--- tcp_client.go
+++ tcp_client.go
@@ -8,26 +8,26 @@ import (
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "172.17.0.3:8000")
+	conn, err := net.Dial("tcp", "172.17.0.2:8000")
 	if err != nil {
 		log.Fatal(err)
 	}
     
 	log.Println("os.Args[1] = ", os.Args[1])
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+	log.Println("Kryptert melding: ", string(kryptertMelding))
+	_, err = conn.Write([]byte(string(kryptertMelding)))
 
- 	_, err = conn.Write([]byte(os.Args[1]))
 	if err != nil {
 		log.Fatal(err)
 	}
+ 	
 	buf := make([]byte, 1024)
 	n, err := conn.Read(buf)
 	if err != nil {
 		log.Fatal(err)
-	} 
-	response := string(buf[:n])
-	log.Printf("reply from proxy: %s", response)
+	}
 
-	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
-	log.Println("Kryptert melding: ", string(kryptertMelding))
-	_, err = conn.Write([]byte(string(kryptertMelding)))
+	response := string(buf[:n])
+	log.Printf("reply from proxy: %s", response)	
 }
